47f98f2f790603b081ebf9a6f53d6ae462722124
gameboy-stack <gt173211@gmail.com>
2021-08-13 12:44:01 +0530

    Basic Stuffs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{ useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App() {
+
+  const [count, setCount] = useState(0);
+
+
+  return (<>
+          <div>
+            <p>Just Click !!!</p>
+            <p>The Number Is : <strong>{ count }</strong></p>
+          </div>
+          <button onClick={() => setCount((count) => count - 1)}>-</button>
+          <span>{ count }</span>
+          <button onClick={() => setCount((count) => count + 1)}>+</button>
+          </>
   );
 }
-
-export default App;
